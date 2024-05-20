c62374b90315c16fd89fc7ce9770efbd41daed3a
RameenKhannnn <rameenkhan8787@gmail.com>
2024-05-20 00:34:11 -0700

    user name

---
+++ app.js
@@ -0,0 +1,35 @@
+// Chapter 5 \\
+var popularNumber = 4;
+var popularNumber = 2 + 2;
+alert(2+2);
+
+// chapter 6 \\
+var newNum = 9
+var num = 1;
+ var newNum = num++;
+
+//  chapter 7 \\
+
+var totalCost = 1 + 3 * 4;
+var totalCost = 1 + (3 * 4);
+var totalCost = (1 + 3) * 4;
+var resultOfComputation = (2 * 4) * 4 + 2;
+resultOfComputation = ((2 * 4) * 4) + 2;
+resultOfComputation = (2 * 4) * (4 + 2);
+
+// chapter 8 \\
+  console.log("2"+ 2); //concatination
+   console.log("3"-1);
+
+ var num = "5"
+   console.log(num + 4 (3+2));
+  // chapter 9 //
+ var userName = +prompt("Enter your Name:3")
+ console.log(userName + 3);
+
+var isStudentCome = true 
+if(isStudentCome){
+     console.log("student aya hai");
+ }else{
+     console.log("nh aya");
+ }
